c4b42b40daf97c4ce5ab954f6e49a4038963b957
Renato Almeida <renato.m.alm@gmail.com>
2017-05-25 22:11:42 +0100

    Project init 😎

---
+++ library/src/main/java/com/youclap/rxfacebook/RxFacebookInitProvider.java
@@ -0,0 +1,61 @@
+package com.youclap.rxfacebook;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Content provider that does not provider any content.
+ * It provides an easy way to initialize this library
+ */
+public class RxFacebookInitProvider extends ContentProvider {
+
+    private static Context mContext;
+
+    /**
+     * Should only be available package level
+     *
+     * @return the context of the application
+     */
+    protected static Context getAppContext() {
+        return mContext;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mContext = getContext();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
